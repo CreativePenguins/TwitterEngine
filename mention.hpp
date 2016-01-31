94b6049a115e185a171e6590afbd6972523e44a3
Tyler Pearson <tpearson1994@gmail.com>
2016-01-31 15:46:54 -0600

    changed file extentions

---
+++ mention.hpp
@@ -0,0 +1,11 @@
+#ifndef mention_h
+#define mention_h
+
+/*******************************************************************************
+ * The purpose of this funciton is to define the class and functions necessary *
+ * to be able to read an analyze mentions in twitter.                          *
+ ******************************************************************************/
+
+
+
+#endif
---
+++ url.hpp
@@ -0,0 +1,11 @@
+#ifndef url_h
+#define url_h
+
+/*******************************************************************************
+ * The purpose of this funciton is to define the class and functions necessary *
+ * to be able to read an analyze URLs in twitter.                          *
+ ******************************************************************************/
+
+
+
+#endif
