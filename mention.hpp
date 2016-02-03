c5f9d78a2c16f93187653a0efbd09d4df9203f34
Tyler Pearson <tpearson1994@gmail.com>
2016-02-02 18:03:23 -0600

    modified mention, started forming class and accessors

--- mention.hpp
+++ mention.hpp
@@ -1,3 +1,4 @@
+#include <string.h>
 #ifndef mention_h
 #define mention_h
 
@@ -6,6 +7,24 @@
  * to be able to read an analyze mentions in twitter.                          *
  ******************************************************************************/
 
+// Need to create class
+
+class Mention {
+    
+private:
+    std::string name; // Just the string that includes the user name.
+    
+public:
+    //constructor
+    Mention();
+    Mention(char at, std::string n);
+    
+    //accessors and mutators
+    void set_name(std::string n); // Function to set the mention
+    void get_name();              // Fucntion to return mention w/o at sign
+    void find_mention();
+    
+};
 
 
 #endif
