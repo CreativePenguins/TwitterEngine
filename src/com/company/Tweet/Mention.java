c5adf82e0d69e9f03f07a85eba6fc66acd3d5113
Tyler Pearson <tpearson1994@gmail.com>
2016-02-04 00:03:02 -0600

    added lists for mentions

--- src/com/company/Tweet/Mention.java
+++ src/com/company/Tweet/Mention.java
@@ -1,5 +1,8 @@
 package com.company.Tweet;
 
+import javax.swing.*;
+import java.util.List;
+
 /**
  * Created by Tyler on 2/3/16.
  */
@@ -8,19 +11,27 @@ public class Mention extends Tweet {
     // This class will just contain the necessary steps for the mention using the @symbol.
 
     private String mention;
+    private List mentions;
 
     // Need, set, get, find.
 
     // I would like this to eventually be an array in case of more than one mention.
     public void setMention(String m) {
-        this.mention = m;
+        mentions.add(m);
     }
 
-    public String getMention() {
-        return mention;
+    public void displayMentions() {
+        int i = 0;
+        while (i < mentions.size()){
+            System.out.print(mentions.get(i));
+            i++;
+        }
     }
 
     public void findMention() {
+
+
+
         return;
     }
 
