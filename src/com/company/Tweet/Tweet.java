b511110763a7e44f8612935cb2ee8e4af6abcb58
Tyler Pearson <tpearson1994@gmail.com>
2016-02-03 22:23:31 -0600

    restarted with a simple tweet class.

--- src/com/company/Tweet/Tweet.java
+++ src/com/company/Tweet/Tweet.java
@@ -4,4 +4,20 @@ package com.company.Tweet;
  * Created by Tyler on 2/3/16.
  */
 public class Tweet {
+
+    /* In this class, all tweets have 3 main things. The mention, topic, and URLs, and sometimes tweets have nothing
+     * The goal of this class is to create a class that will be able to look through the tweets and find, based on
+     * markers such as @, #, and http://, and identify until white space is reached.
+     */
+
+    // Below are the things needed for tweets, the three main things.
+    private String mention;
+    private String topic;
+    private String url;
+
+    public Tweet() {
+    }
+
+    // Below are the functions I'm going to need.
+
 }
