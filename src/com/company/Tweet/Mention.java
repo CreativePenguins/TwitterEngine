516521820a5c181b4d98db50d4ce01a130d92608
Tyler Pearson <tpearson1994@gmail.com>
2016-02-03 23:17:27 -0600

    skeleton, scary skeleton.

--- src/com/company/Tweet/Mention.java
+++ src/com/company/Tweet/Mention.java
@@ -3,5 +3,25 @@ package com.company.Tweet;
 /**
  * Created by Tyler on 2/3/16.
  */
-public class Mention {
+public class Mention extends Tweet {
+
+    // This class will just contain the necessary steps for the mention using the @symbol.
+
+    private String mention;
+
+    // Need, set, get, find.
+
+    // I would like this to eventually be an array in case of more than one mention.
+    public void set_mention(String m) {
+        this.mention = m;
+    }
+
+    public String get_mention() {
+        return mention;
+    }
+
+    public void find_mention() {
+        return;
+    }
+
 }
