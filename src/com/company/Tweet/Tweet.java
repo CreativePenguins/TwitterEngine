de53dba3c1667cfeab9cb376c6da869c4517205f
Tyler Pearson <tpearson1994@gmail.com>
2016-02-15 21:17:49 -0600

    non compiling code, just commiting to prevent catastrophe later down the road

--- src/com/company/Tweet/Tweet.java
+++ src/com/company/Tweet/Tweet.java
@@ -1,5 +1,8 @@
 package com.company.Tweet;
 
+import java.util.List;
+import java.util.regex.*;
+
 /**
  * Created by Tyler on 2/3/16.
  */
@@ -13,8 +16,9 @@ public class Tweet {
     // In just this class we will just have the message.
 
     private String message;
-
-    public Tweet(){}
+    private List mentions;
+    private List topics;
+    private List urls;
 
     public Tweet(String m) {
         setMessage(m);
@@ -27,5 +31,51 @@ public class Tweet {
     public String getMessage() {
         return message;
     }
+
+    public void displayMentions() {
+        for (int i = 0; i < mentions.size(); ++i)
+            System.out.print(mentions.get(i));
+    }
+
+    public void findMentions() {
+        int i = 0;
+        int x;
+        while (i < message.length()) {
+            if (message.charAt(i) == '@') {
+                x = i;
+                while (message.charAt(x) != ' ') {
+                    x++;
+                }
+                mentions.add(message.substring(i + 1, x));
+                i = x + 1;
+            }
+            i++;
+        }
+        return;
+    }
+
+    public void addTopic(String t) {
+        topics.add(t);
+    }
+
+    public void displayTopics() {
+        for (int i = 0; i < topics.size(); ++i)
+            System.out.print(topics.get(i).toString());
+    }
+
+    public void findTopics() {
+        int i = 0;
+        int x;
+        while (i<message.length()) {
+            if (message.charAt(i) == '#') {
+                x = i;
+                while (message.charAt(x) != ' ') {
+                    x++;
+                } topics.add(message.substring(i+1,x));
+                i = x+1;
+            }i++;
+        }return;
+    }
+
 
 }
